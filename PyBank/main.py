a9eea26d25c517517952e81db8b3197206c65e5a
TravisDC5 <61341968+TravisDC5@users.noreply.github.com>
2020-03-26 15:57:41 -0700

    Behzingaa, got month count and total profit/loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,24 @@
 import os
 import csv
 monthsCount = 0
+proflossTotal = 0
+
 datapath = os.path.join('resources', 'BudgetData.csv')
 
 with open(datapath) as csvfile:
 
     dataparser = csv.reader(csvfile, delimiter=',')
-
+    
     csv_header = next(dataparser)
 
-    """for row in dataparser:
-        'monthsCount = len(row[0])
-    """  
-    monthsCount = len(list(dataparser))
+    
 
-    print(f"Total Amount of Months: {monthsCount}")
+    for row in dataparser:
         
+        monthsCount += 1
+        proflossTotal = proflossTotal + int(row[1])
+    
+    
+    
+    print(f"Total Amount of Months: {monthsCount}")
+    print(f"Total Profit/Loss = ${proflossTotal}")    
