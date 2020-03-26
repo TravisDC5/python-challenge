68e2c2f9f39716daed9d03ed99f9aa9520155849
TravisDC5 <61341968+TravisDC5@users.noreply.github.com>
2020-03-26 11:27:52 -0700

    read in csv and output number of dates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
 import os
 import csv
-
+monthsCount = 0
 datapath = os.path.join('resources', 'BudgetData.csv')
 
 with open(datapath) as csvfile:
@@ -9,6 +9,10 @@ with open(datapath) as csvfile:
 
     csv_header = next(dataparser)
 
-    for row in dataparser:
-        print(row[0])
+    """for row in dataparser:
+        'monthsCount = len(row[0])
+    """  
+    monthsCount = len(list(dataparser))
+
+    print(f"Total Amount of Months: {monthsCount}")
         
