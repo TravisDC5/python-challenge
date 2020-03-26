1d26e0acbe639f2c4a3bfcb63da1b5a70a25a11f
TravisDC5 <61341968+TravisDC5@users.noreply.github.com>
2020-03-26 10:59:28 -0700

    data read in error

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,14 @@
 import os
 import csv
 
-datapath = os.path.join('g','DataBootCamp','PythonWork','PyBank','resources', '03-Python_HW_Instructions_PyBank_Resources_budget_data.csv')
+datapath = os.path.join('g','DataBootCamp','PythonWork','python-challenge','PyBank','resources', 'BudgetData.csv')
 
+with open(datapath) as csvfile:
+
+    dataparser = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(dataparser)
+
+    for row in dataparser:
+        print(row[0])
+        
