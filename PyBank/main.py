aeb4365720d792cf29a2b08fec2d84ef664f8099
TravisDC5 <61341968+TravisDC5@users.noreply.github.com>
2020-03-26 16:25:07 -0700

    min max comparison

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,15 +10,22 @@ with open(datapath) as csvfile:
     dataparser = csv.reader(csvfile, delimiter=',')
     
     csv_header = next(dataparser)
-
-    
+    'minValue, maxValue = [], []'
+    dataArray = []
 
     for row in dataparser:
         
         monthsCount += 1
         proflossTotal = proflossTotal + int(row[1])
+        'minValue.append(row[1])'
+        'maxValue.append(row[1])'
+        dataArray.append(row[1])
+
+
     
     
-    
-    print(f"Total Amount of Months: {monthsCount}")
-    print(f"Total Profit/Loss = ${proflossTotal}")    
+    '''print(f"Total Amount of Months: {monthsCount}")
+    print(f"Total Profit/Loss = ${proflossTotal}") 
+    print(min(dataArray))
+    print(max(dataArray))'''
+    print(dataArray)   
